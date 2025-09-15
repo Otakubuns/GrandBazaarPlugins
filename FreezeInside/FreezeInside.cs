0c17af841496413ac3e0b0884e07f71949a4a609
Alexus <alexusgirl12@gmail.com>
2025-09-15 18:43:39 -0400

    added helper function so im not yandere deving it

--- FreezeInside/FreezeInside.cs
+++ FreezeInside/FreezeInside.cs
@@ -19,9 +19,8 @@ public class FreezeInside : BasePlugin
 
     public override void Load()
     {
-        _timeScaleValue = Config.Bind("General", "Time Scale", 60.0f, "Set the time scale of the game(default is a second per in-game minute). Game default is 60.0f.");
+        _timeScaleValue = Config.Bind("General", "Time Scale", 60.0f, "Set the time scale of the game(default is a second per in-game minute). Game default is 60.0.");
         _freezeInside = Config.Bind("General", "Freeze Inside", true, "Set to true to freeze time when indoors.");
-        // Plugin startup logic
         Log = base.Log;
         Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
         Harmony.CreateAndPatchAll(typeof(TimeFreezePatch));
@@ -37,34 +36,8 @@ public class FreezeInside : BasePlugin
         [HarmonyPostfix]
         private static void OnChangeField(FieldManager __instance, FieldMasterId fieldId)
         {
-            SingletonMonoBehaviour<DateManager>.Instance.TimeScale =
-                MasterDataManager.Instance.FieldMaster.GetData(fieldId).IsInDoor ? 0 : 60;
-        }
-
-        [HarmonyPatch(typeof(DateManager), "OnStartGame")]
-        [HarmonyPostfix]
-        private static void OnStartGame(DateManager __instance)
-        {
-            try
-            {
-                if (_freezeInside.Value)
-                {
-                    SingletonMonoBehaviour<DateManager>.Instance.TimeScale =
-                        MasterDataManager.Instance.FieldMaster
-                            .GetData(SingletonMonoBehaviour<FieldManager>.Instance.CurrentFieldId).IsInDoor
-                            ? 0
-                            : _timeScaleValue.Value;
-
-                }
-                else
-                {
-                    SingletonMonoBehaviour<DateManager>.Instance.TimeScale = _timeScaleValue.Value;
-                }
-            }
-            catch (Exception e)
-            {
-                // ignored
-            }
+            var isIndoors = MasterDataManager.Instance.FieldMaster.GetData(fieldId).IsInDoor;
+            UpdateTimeScale(isIndoors);
         }
         
         /**
@@ -76,21 +49,21 @@ public class FreezeInside : BasePlugin
         {
             try
             {
-                if (_freezeInside.Value)
-                {
-                    SingletonMonoBehaviour<DateManager>.Instance.TimeScale =
-                        MasterDataManager.Instance.FieldMaster.GetData(__instance.CurrentFieldId).IsInDoor ? 0 : _timeScaleValue.Value;
-                }
-                else
-                {
-                    SingletonMonoBehaviour<DateManager>.Instance.TimeScale = _timeScaleValue.Value;
-                }
-
+                var isIndoors = MasterDataManager.Instance.FieldMaster.GetData(__instance.CurrentFieldId).IsInDoor;
+                UpdateTimeScale(isIndoors);
             }
             catch (Exception e)
             {
                 // ignored
             }
+        }
+        
+        /*
+         * Helper function to update timescale(so im not doing too many if else's)
+         */
+        private static void UpdateTimeScale(bool isIndoors)
+        {
+            SingletonMonoBehaviour<DateManager>.Instance.TimeScale = _freezeInside.Value && isIndoors ? 0 : _timeScaleValue.Value;
         }
     }
 }
