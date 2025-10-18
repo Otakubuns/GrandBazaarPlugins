6bad009eabf1afb8c8248ae84f22476b36695f79
Alexus <alexusgirl12@gmail.com>
2025-10-18 14:19:11 -0400

    (W.I.P.) Cooking ui done & redone storage amount logic

--- CraftFromStorage/CraftFromStorage.cs
+++ CraftFromStorage/CraftFromStorage.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Linq;
+
 using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
+using BepInEx.Unity.IL2CPP.UnityEngine;
 using BokuMono;
 using BokuMono.Data;
 using BokuMono.FieldEvent;
@@ -12,6 +13,9 @@ using Il2CppSystem.Collections.Generic;
 using Il2CppSystem.Text;
 using UnityEngine;
 using UnityEngine.UI;
+using KeyCode = BepInEx.Unity.IL2CPP.UnityEngine.KeyCode;
+using Logger = BepInEx.Logging.Logger;
+using Math = System.Math;
 using Object = UnityEngine.Object;
 
 namespace CraftFromStorage;
@@ -31,7 +35,45 @@ public class CraftFromStorage : BasePlugin
 
     private class CraftingPatch
     {
-        private static bool _hasPatched;
+        /*
+         * This patch is to add the extra text showing how many items are in storage under the bag amount for cooking
+         */
+        [HarmonyPatch(typeof(UICookingPage), "InitPage")]
+        [HarmonyPostfix]
+        private static void PatchCookingUI(UICookingPage __instance)
+        {
+            //UICookingDetail and go to the itemselector from there
+            var cookingItemSelector = __instance.cookingDetail.requiredItemSelector;
+
+            if (cookingItemSelector == null) return;
+
+            //just check the first one to see if its patched
+            if (cookingItemSelector.transform.GetChild(2).GetChild(0).GetChild(0).FindChild("HaveStorageStackBG") !=
+                null) return;
+            
+            //TODO:
+            // var rectangle = cookingItemSelector.GetComponent<RectTransform>();
+            // rectangle.sizeDelta = new Vector2(rectangle.sizeDelta.x, rectangle.sizeDelta.y + 90);
+            // cookingItemSelector.transform.position += new Vector3(0, 0.07f, 0);
+
+            // Group(Cooking)
+            var groupGameObject = cookingItemSelector.transform.GetChild(2).gameObject;
+            // var child = groupGameObject.GetComponent<RectTransform>();
+            //
+            // child.anchoredPosition = new Vector2(child.anchoredPosition.x, child.anchoredPosition.y + 45);
+
+            var childCount = groupGameObject.transform.childCount;
+
+            // Creating new icons for the storage amount text by copying the original ui element and placing underneath
+            for (var i = 0; i < childCount; i++)
+            {
+                var detailObject = groupGameObject.transform.GetChild(i).gameObject;
+                var ui = detailObject.transform.GetChild(0).GetChild(3).gameObject;
+                var storageStackBg = Object.Instantiate(ui, ui.transform.parent, true);
+                storageStackBg.name = "HaveStorageStackBG";
+                storageStackBg.transform.position = ui.transform.position + new Vector3(0, -0.07f, 0);
+            }
+        }
 
         /*
          * This patch is to add the extra text showing how many items are in storage under the bag amount
@@ -40,52 +82,50 @@ public class CraftFromStorage : BasePlugin
         [HarmonyPostfix]
         private static void CreateStorageStack(UIWindmillCraftingPage __instance)
         {
-            //TODO: Add a hook for going to menu if this is wiped on main menu
-            if (_hasPatched) return;
-            // this is the ui elemet of holding the 5 icons and item amount
-            var backgroundImage =
-                GameObject.Find(
-                    "UserInterface(Clone)/UICamera/UIMenuCanvas/UIMenuManager/Pages/UIWindmillCraftingPage(Clone)/UIWindmillCraftingScrollGroup/UIWindmillCraftingDetail/UIRequiredItemSelector(Windmill)/");
+            // this is the ui element of holding the 5 icons and item amount
+            var requiredItemSelector = __instance.windmillCraftingDetail.requiredItemSelector;
 
+            if (requiredItemSelector == null) return;
 
-            if (backgroundImage == null) return;
+            //just check the first one to see if its patched
+            if (requiredItemSelector.transform.GetChild(1).GetChild(0).GetChild(0).FindChild("HaveStorageStackBG") !=
+                null) return;
+            
+             var rectangle = requiredItemSelector.GetComponent<RectTransform>();
+             rectangle.sizeDelta = new Vector2(rectangle.sizeDelta.x, rectangle.sizeDelta.y + 90);
+             requiredItemSelector.transform.position += new Vector3(0, 0.07f, 0);
 
-            var groupGameObject = backgroundImage.transform.GetChild(1).gameObject;
-
-            if (groupGameObject == null) return;
-
-            var rectangle = backgroundImage.GetComponent<RectTransform>();
-            rectangle.sizeDelta = new Vector2(rectangle.sizeDelta.x, rectangle.sizeDelta.y + 90);
-            backgroundImage.transform.position += new Vector3(0, 0.07f, 0);
-
-            var child = backgroundImage.transform.GetChild(1).GetComponent<RectTransform>();
-
+            // Group(Windmill) holds the 5 icons and amount
+            var groupGameObject = requiredItemSelector.transform.GetChild(1).gameObject;
+            var child = groupGameObject.GetComponent<RectTransform>();
             child.anchoredPosition = new Vector2(child.anchoredPosition.x, child.anchoredPosition.y + 45);
-
-            var uiWindmillDetail = __instance.transform.GetChild(0).GetChild(3).gameObject;
-            var icon = uiWindmillDetail.transform.GetChild(2).gameObject;
+            
+            var uiDetail = __instance.transform.GetChild(0).GetChild(3).gameObject;
+            var icon = uiDetail.transform.GetChild(2).gameObject;
             icon.transform.position += new Vector3(0, 0.04f, 0);
-            var category = uiWindmillDetail.transform.GetChild(3).gameObject;
+            var category = uiDetail.transform.GetChild(3).gameObject;
             category.transform.position += new Vector3(0, 0.15f, 0);
 
+            //get amount of children
+            var childCount = groupGameObject.transform.childCount;
+
             // Creating 5 new icons for the storage amount text by copying the original ui element and placing underneath
-            for (var i = 0; i < 5; i++)
+            for (var i = 0; i < childCount; i++)
             {
-                var detailObject = groupGameObject.transform.GetChild(i);
+                var detailObject = groupGameObject.transform.GetChild(i).gameObject;
                 var ui = detailObject.transform.GetChild(0).GetChild(3).gameObject;
                 var storageStackBg = Object.Instantiate(ui, ui.transform.parent, true);
                 storageStackBg.name = "HaveStorageStackBG";
                 storageStackBg.transform.position = ui.transform.position + new Vector3(0, -0.07f, 0);
             }
-
-            _hasPatched = true;
         }
-        
+
+
         /*
          * This patch makes the recipe icons to be craftable if the items are in storage
          * Does not allow crafting from storage yet, just makes the icon highlighted and clickable
          */
-        [HarmonyPatch(typeof(RequiredItemMaster), nameof(RequiredItemMaster.IsEnough))]
+        [HarmonyPatch(typeof(RequiredItemMaster), "IsEnough")]
         [HarmonyPostfix]
         public static void PatchRecipeMask(BokuMono.StorageManager __0,
             BokuMono.Data.IRequiredItemMasterData __1,
@@ -93,82 +133,7 @@ public class CraftFromStorage : BasePlugin
         {
             if (__result) return; // if already true no real need to check
 
-            __result = IsCraftable(__1);
-        }
-
-        /*
-         * This function will check if a recipe is craftable from storage and bag
-         * @param itemMasterData - the recipe to check(holds ingredient info only, not the recipe info)
-         * returns true if it is
-         */
-        private static bool IsCraftable(IRequiredItemMasterData itemMasterData)
-        {
-            var inventoryManager = ManagedSingleton<InventoryManager>.Instance;
-
-            for (int i = 0; i < itemMasterData.RequiredItemCount; i++)
-            {
-                var itemData = itemMasterData.RequiredItemList._items[i].ToString().Split(',');
-                var itemId = uint.Parse(itemData[0].Trim('(', ' '));
-                var stack = int.Parse(itemData[1].Trim(' ', ')'));
-                var category = itemMasterData.RequiredItemTypeList._items[i];
-
-                if (itemId == 0 || stack == 0) continue;
-
-                int CountInAllStorages(Func<ItemData, bool> predicate)
-                {
-                    return inventoryManager.BagItemStorage.itemDatas
-                               .Where(predicate).Sum(x => x.Stack)
-                           + inventoryManager.HouseStorage.itemDatas
-                               .Where(predicate).Sum(x => x.Stack)
-                           + inventoryManager.BagToolStorage.itemDatas
-                               .Where(predicate).Sum(x => x.Stack);
-                }
-
-                switch (category)
-                {
-                    case RequiredItemType.Item:
-                        var totalAmount = CountInAllStorages(x =>
-                            x.ItemId == itemId);
-
-                        if (totalAmount < stack)
-                        {
-                            return false;
-                        }
-
-                        break;
-
-                    case RequiredItemType.Category:
-                        var totalCategoryAmount = CountInAllStorages(x =>
-                            x.Category == itemId);
-
-                        if (totalCategoryAmount < stack)
-                        {
-                            return false;
-                        }
-
-                        break;
-
-                    case RequiredItemType.Group:
-                        if (!itemMasterData.GroupMaster.TryGetGroupData(itemId, out var groupData) ||
-                            groupData == null) continue;
-
-                        foreach (var requiredItem in groupData.RequiredItemIdList)
-                        {
-                            if (requiredItem == 0) continue;
-
-                            var totalGroupAmount = CountInAllStorages(x => x.ItemId == requiredItem);
-
-                            if (totalGroupAmount < stack)
-                            {
-                                return false;
-                            }
-                        }
-
-                        break;
-                }
-            }
-
-            return true;
+            __result = CraftingHelper.IsCraftable(__1);
         }
 
         /*
@@ -183,55 +148,58 @@ public class CraftFromStorage : BasePlugin
             var inventoryManager = ManagedSingleton<InventoryManager>.Instance;
             if (inventoryManager == null) return;
 
+            var childIndex = 1;
 
-            var counterForIcons = 0;
-            // its never gonna be more than 1 as im now grabbing single ingredient
-            foreach (var tuple in data.RequiredItemList)
+            if (__instance.name == "UIRequiredItemSelector(Cooking)")
             {
-                var itemString = tuple.ToString();
-                var split = itemString.Split(',');
-                var itemId = uint.Parse(split[0].Trim('(', ' '));
-                var stack = int.Parse(split[1].Trim(' ', ')'));
+                childIndex = 2;
+            }
+
+            // this is the data holding the required items for the recipe (item id, stack)
+            var childCount = __instance.transform.GetChild(childIndex).childCount;
+
+            for (int i = 0; i < childCount; i++)
+            {
+                var itemData = data.RequiredItemList._items[i].ToString().Split(',');
+                var itemId = uint.Parse(itemData[0].Trim('(', ' '));
+                var stack = int.Parse(itemData[1].Trim(' ', ')'));
+                var category = data.RequiredItemTypeList._items[i];
+
                 LocalizedTextMeshPro textMesh;
 
                 try
                 {
-                    textMesh = __instance.transform.GetChild(1).GetChild(counterForIcons).GetChild(0)
-                        .FindChild("HaveStorageStackBG").GetChild(0).GetComponent<LocalizedTextMeshPro>();
+                    textMesh = __instance.transform.GetChild(childIndex).GetChild(i).GetChild(0)
+                        .FindChild("HaveStorageStackBG").FindChild("HavedStackText")
+                        .GetComponent<LocalizedTextMeshPro>();
                 }
-                catch (System.Exception e)
+                catch (Exception e)
                 {
-                    counterForIcons++;
                     continue;
                 }
 
                 if (itemId == 0 || stack == 0)
                 {
                     textMesh.text = "";
-                    counterForIcons++;
                     continue;
                 }
 
-
-                var storageAmount = inventoryManager.HouseStorage.itemDatas
-                    .Where(x => x.ItemId == itemId).Sum(x => x.Stack);
+                var storageAmount = CraftingHelper.GetStorageAmount(itemId, category, data.GroupMaster);
 
                 // This patches the little check icon to show if you have enough in storage(or bag)
                 if (storageAmount >= stack)
                 {
-                    __instance.requiredItemIcon._items[counterForIcons].checkIcon.enabled = true;
+                    __instance.requiredItemIcon._items[i].checkIcon.enabled = true;
                 }
 
                 // if amount is over 999 just show 999+
                 if (storageAmount > 999)
                 {
                     textMesh.text = "999+";
-                    counterForIcons++;
                     continue;
                 }
 
                 textMesh.text = storageAmount.ToString();
-                counterForIcons++;
             }
         }
 
@@ -242,33 +210,76 @@ public class CraftFromStorage : BasePlugin
         // 1. have it have the ui for items from stoage(would require alot more work)
         // 2. if they have the item, just popup the amount and then when clicked craft then grab the items then from both bag & storage
 
-        [HarmonyPatch(typeof(UIWindmillCraftingPage), "OnDecide")]
-        [HarmonyPrefix]
-        private static bool OnCraftDecide(UIWindmillCraftingPage __instance, RecipeIconData __0)
+        // This is called when the user clicks on the recipe to craft
+        [HarmonyPatch(typeof(UIRequiredItemSelectPage), "OnShow")]
+        [HarmonyPostfix]
+        private static void OnShowRequiredItemSelect(UIRequiredItemSelectPage __instance)
         {
-            var accessor = UIAccessor.Instance;
+            if (__instance == null) return;
 
 
-            // //Make this open up just the craft count dialog
-            //  try
-            //  {
-            //      UIAccessor.Instance.RequestOpenMenu(UILoadKey.WindmillCraftCountDialog, __0, true);
-            //  }
-            //  catch (Exception e)
-            //  {
-            //      return true;
-            //  }
-            // //Will change this later to add cooking but for now just windmill
-            // if (__0.RecipeType != RecipeMasterType.Windmill) return true;
+            // //requireditemdata the count will always be 5(even if tehre is only 1 item)
+            // var recipeData = __instance.curRecipeMasterData;
+            // var itemSelector = __instance.curDetail.requiredItemSelector;
+            // //itemSelector.selectIndex = itemSelector.maxArrangeItemCount + itemSelector.maxRequiredItemCount;
+            // __instance.OpenCraftDialog(__instance.curRecipeMasterData);
+            // //currequireditemcount is how many different items are needed
             //
-            // if (!IsCraftable(__0.WindmillCraftData.requiredItemList)) return true;
+            // var selector = __instance.curDetail.requiredItemSelector;
+            // var storage = ManagedSingleton<InventoryManager>.Instance; // or whatever you use
             //
-            // var uiData = __0.WindmillCraftData;
-            // var uiAccessor = UIAccessor.Instance;
-            // SoundManager.Instance.Play(3001, SoundManager.Category.Se, 0, false, 1f);
-            // uiAccessor.RequestOpenMenu(UILoadKey.RequiredItemSelect, uiData, true);
-            return true;
+            // foreach (var req in selector.RequiredItems)
+            // {
+            //     if (req == null) continue;
+            //
+            //     // Get all matching items from both bag and storage
+            //     var matches = CraftingHelper.GetMatchingItemsFromAllStorages(req);
+            // }
+
+
+            // if (__instance.curRecipeMasterData == null) return;
+            // try
+            // {
+            //     // __instance.SetActive(false);
+            //     __instance.OpenCraftDialog(__instance.curRecipeMasterData);
+            // }
+            // catch (Exception e)
+            // {
+            //     return;
+            // }
         }
+
+        // [HarmonyPatch(typeof(UIWindmillCraftingPage), "OnDecide")]
+        // [HarmonyPrefix]
+        // private static bool OnCraftDecide(UIWindmillCraftingPage __instance, RecipeIconData __0)
+        // {
+        //     var craftData = __0.WindmillCraftData;
+        //
+        //
+        //     var accessor = UIAccessor.Instance;
+        //     
+        //
+        //
+        //     // //Make this open up just the craft count dialog
+        //     //  try
+        //     //  {
+        //     //      UIAccessor.Instance.RequestOpenMenu(UILoadKey.WindmillCraftCountDialog, __0, true);
+        //     //  }
+        //     //  catch (Exception e)
+        //     //  {
+        //     //      return true;
+        //     //  }
+        //     // //Will change this later to add cooking but for now just windmill
+        //     // if (__0.RecipeType != RecipeMasterType.Windmill) return true;
+        //     //
+        //     // if (!IsCraftable(__0.WindmillCraftData.requiredItemList)) return true;
+        //     //
+        //     // var uiData = __0.WindmillCraftData;
+        //     // var uiAccessor = UIAccessor.Instance;
+        //     // SoundManager.Instance.Play(3001, SoundManager.Category.Se, 0, false, 1f);
+        //     // uiAccessor.RequestOpenMenu(UILoadKey.RequiredItemSelect, uiData, true);
+        //     return true;
+        // }
 
         // [HarmonyPatch(typeof(UIWindmillCraftCountDialog), "GetUIPageData")]
         // [HarmonyPrefix]
@@ -288,8 +299,6 @@ public class CraftFromStorage : BasePlugin
 
         //UIRequiredItemSelectPage.Decide -> When someone selects an item to add to the items
         //INIT IS AFTER GETUIPAGEDATA FOR SOME REASON
-
-        //[HarmonyPatch(typeof(UIRequiredItemSelectPage), "OnDecide")]
 
 
         // Commented out for now as i want to go a different approach atm but would love to have a whole new UI to select stuff from storage
