cf2bcf749a053dd1373cff635211f34d9edeb096
Alexus <alexusgirl12@gmail.com>
2025-11-02 17:36:48 -0500

    SaveAnywhere updated with location loading & JSON handling

---
+++ SaveAnywhere/JsonUtility.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using BepInEx;
+using UnityEngine;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace SaveAnywhere;
+
+/*
+ * Utility class for saving and loading position data to/from a JSON file.
+ */
+public static class JsonUtility
+{
+    private static readonly string FolderPath = Path.Combine(Paths.BepInExRootPath, "plugins", "SaveAnywhere");
+    private static readonly string FilePath = Path.Combine(FolderPath, "SaveAnywhere.json");
+
+    /*
+     * Loads the list of SaveData from the JSON file.
+     * Returns an empty list if the file does not exist or if deserialization fails.
+     */
+    private static List<SaveData> LoadSaveDatas()
+    {
+        if (Directory.Exists(FolderPath) == false)
+        {
+            Directory.CreateDirectory(FolderPath);
+        }
+
+        //Check if folder exists
+        if (!File.Exists(FilePath))
+        {
+            return [];
+        }
+
+        var json = File.ReadAllText(FilePath);
+        var saveDataList = JsonSerializer.Deserialize<List<SaveData>>(json);
+        return saveDataList ?? [];
+    }
+
+    /*
+     * Saves the given position to the JSON file under the specified slot.
+     * If the slot already exists, it updates the position; otherwise, it adds a new entry.
+     */
+    public static void SaveDataJson(Vector3 position, int slot)
+    {
+        try
+        {
+            // -1 slot is the auto save
+            if (slot < -1) return;
+            var saveDataList = LoadSaveDatas();
+
+            // I could create a new class for the Vector to store better but tbh this is simpler and it's not big
+            var existingData = saveDataList.FirstOrDefault(data => data.Slot == slot);
+            var positionData = new Vector3Data(position.x, position.y, position.z);
+            if (existingData != null)
+            {
+                existingData.Position = positionData;
+            }
+            else
+            {
+                saveDataList.Add(new SaveData {Slot = slot, Position = positionData});
+            }
+
+            // Ordering it by id so it's easier to read
+            saveDataList = saveDataList.OrderBy(data => data.Slot).ToList();
+
+            var json = JsonSerializer.Serialize(saveDataList, new JsonSerializerOptions {WriteIndented = true});
+            File.WriteAllText(FilePath, json);
+        }
+        catch (Exception e)
+        {
+            SaveAnywhere.LOG.LogError("Failed to save data to JSON: " + e.Message);
+        }
+    }
+
+
+    public static Vector3 LoadDataJson(int slot)
+    {
+        try
+        {
+            var saveDataList = LoadSaveDatas();
+            var existingData = saveDataList.FirstOrDefault(data => data.Slot == slot);
+            if (existingData != null)
+            {
+                var positionString = existingData.Position;
+                return new Vector3(positionString.X, positionString.Y, positionString.Z);
+            }
+        }
+        catch (Exception e)
+        {
+            SaveAnywhere.LOG.LogError("Failed to load data from JSON: " + e.Message);
+        }
+
+        return new Vector3(0, 0, 0);
+    }
+}
+
+public class SaveData
+{
+    public int Slot { get; init; }
+    public Vector3Data Position { get; set; }
+}
+
+public class Vector3Data
+{
+    public float X { get; }
+    public float Y { get; }
+    public float Z { get; }
+
+    public Vector3Data(float x, float y, float z)
+    {
+        //round to 2 decimal places to reduce file size
+        X = (float) Math.Round(x, 4);
+        Y = (float) Math.Round(y, 4);
+        Z = (float) Math.Round(z, 4);
+    }
+}
--- SaveAnywhere/SaveAnywhere.cs
+++ SaveAnywhere/SaveAnywhere.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx;
+﻿using System.Text.Json;
+using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using BokuMono;
@@ -12,22 +13,23 @@ namespace SaveAnywhere;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class SaveAnywhere : BasePlugin
 {
-    static ManualLogSource _log;
-    
+    public static ManualLogSource LOG;
+
     public override void Load()
     {
-        _log = Log;
+        LOG = Log;
         // Plugin startup logic
-        _log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        LOG.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 
         Harmony.CreateAndPatchAll(typeof(SavePatch));
     }
     
-    //TODO: add maybe a json for location and load for the save so they can on save be there
-    
-    [HarmonyPatch]
     private class SavePatch
     {
+        // Track if a save has just been loaded & the current loaded save slot
+        private static bool _isLoadedSave;
+        private static int _currentSlot = -2;
+        
         /**
          * This patch overrides the save button logic and opens the save/load menu instead of an error if not in the farmhouse
          */
@@ -37,7 +39,7 @@ public class SaveAnywhere : BasePlugin
             List<StateErrorTextId> __2)
         {
             //if the save button(1130 text id) is pressed do custom logic, if not just return the original method
-            //or index depdending on the circumstance
+            //or index depending on the circumstance
             //if(__0 != 0) return true;
             if (__1 != 1130) return true;
             __2?.Clear();
@@ -49,7 +51,7 @@ public class SaveAnywhere : BasePlugin
         }
 
         /**
-         * This patch removes the error id in dialogdata so the save button is always enabled
+         * This patch removes the error id in DialogData so the save button is always enabled
          */
         [HarmonyPatch(typeof(UIPausePage), "DialogChoices")]
         [HarmonyPostfix]
@@ -60,5 +62,85 @@ public class SaveAnywhere : BasePlugin
                 choice.errorIds?.Clear();
             }
         }
+        
+        /*
+         * This patch sets the players position after loading a save based on the saved json data
+         * It checks if a save was loaded and the slot id first and only fires then
+         */
+        [HarmonyPatch(typeof(PlayerCharacter), "SetPlayerPosition")]
+        [HarmonyPostfix]
+        public static void SetPlayerPositionPatch(PlayerCharacter __instance)
+        {
+            if (!_isLoadedSave || _currentSlot < -1) return;
+             
+            try
+            {
+                if (__instance == null) return;
+                
+                var position = JsonUtility.LoadDataJson(_currentSlot);
+                SetPlayerPosition(__instance, position, Vector3.zero);
+                _isLoadedSave = false;
+            }
+            catch (System.Exception e)
+            {
+                LOG.LogError("Failed to load position from json: " + e.Message + "\n" + e.StackTrace);
+            }
+        }
+        
+        /*
+         * This patch tracks when a save is loaded and the slot id and stores it for when the player position is set
+         */
+        [HarmonyPatch(typeof(SaveLoadManager), "LoadSlot")]
+        [HarmonyPostfix]
+        public static void LoadLocation(SaveLoadManager __instance, int iSlot)
+        {
+            _isLoadedSave = true;
+            _currentSlot = iSlot;
+        }
+        
+        /*
+         * This patch saves the player position to json when a save is made, so it can be loaded on load
+         */
+        [HarmonyPatch(typeof(SaveLoadManager), "Save")]
+        [HarmonyPostfix]
+        private static void SaveLocation(SaveLoadManager __instance, int iSlot)
+        {
+            var player = GameController.Instance.playerCharacter;
+            if (player == null) return;
+            var position = player.transform.position;
+            JsonUtility.SaveDataJson(position, iSlot);
+        }
+    }
+    
+    /*
+     * This is a simple recreation of the PlayerCharacter SetPlayerPosition method to set the player position
+     * The original was not working, so this is a replacement
+     */
+    private static void SetPlayerPosition(PlayerCharacter player, Vector3 position, Vector3 rotation)
+    {
+        if (position == new Vector3(0, 0, 0)) return;
+        player.SetEnableMagicaCloth(true);
+
+        var euler = Vector3.zero;
+        var rot = Quaternion.Euler(euler);
+
+        if (player.transformCache != null)
+            player.transformCache.SetPositionAndRotation(position, rot);
+
+        if (player.charaRigidbody == null) return;
+        player.charaRigidbody.position = position;
+        player.charaRigidbody.rotation = rot;
+
+        if (player._Model_k__BackingField == null) return;
+
+        var model = player._Model_k__BackingField;
+        var tr = model.transform;
+        if (tr != null)
+            tr.SetPositionAndRotation(position, rot);
+
+        player.SetPrevWaistPosition();
+
+        if (player.actionCollider == null) return;
+        player.actionCollider.ClearCache();
     }
 }
