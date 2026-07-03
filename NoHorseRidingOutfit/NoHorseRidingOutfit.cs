50b8e8db201616cee1ba4a72dc26b1d37f6cb8cf
Alexus <alexusgirl12@gmail.com>
2026-07-03 17:09:39 -0400

    Added NoHorseRidingOutfit

---
+++ NoHorseRidingOutfit/NoHorseRidingOutfit.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using BokuMono;
+using BokuMono.MiniGame;
+using HarmonyLib;
+
+namespace NoHorseRidingOutfit;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+    private static ConfigEntry<bool> ShowOutfitOnDerby;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        ShowOutfitOnDerby = Config.Bind("Settings", "DerbyOutfit", false, "If true, the riding outfit will be shown in the derby mini-game");
+        Harmony.CreateAndPatchAll(typeof(HorseRidingPatch));
+    }
+
+    public class HorseRidingPatch
+    {
+        [HarmonyPatch(typeof(PlayerCharacter), "ChangeIntoRidingClothes")]
+        [HarmonyPrefix]
+        static void PatchRidingClothes(ref bool set)
+        {
+            if (MiniGameManager.Instance.CurrentType == MiniGameType.HorseRacing && ShowOutfitOnDerby.Value) return;
+            set = false;
+        }
+    }
+}
