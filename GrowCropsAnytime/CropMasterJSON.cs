8dd41ec1ae40fec5be8f2481f9e8c3e31c53b95d
Alexus <alexusgirl12@gmail.com>
2026-08-10 22:42:12 -0400

    Added new plugins

---
+++ GrowCropsAnytime/CropMasterJSON.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text.Json;
+using HarmonyLib;
+
+namespace GrowCropsAnytime;
+
+public class CropMasterJSON
+{
+    public class CropConfig{
+        public uint CropId { get; set; }
+        public bool GrowInSpring { get; set; }
+        public bool GrowInSummer { get; set; }
+        public bool GrowInAutumn { get; set; }
+        public bool GrowInWinter { get; set; }
+
+        public CropConfig(uint cropId, bool growInSpring, bool growInSummer, bool growInAutumn, bool growInWinter)
+        {
+            CropId = cropId;
+            GrowInSpring = growInSpring;
+            GrowInSummer = growInSummer;
+            GrowInAutumn = growInAutumn;
+            GrowInWinter = growInWinter;
+        }
+    }
+    
+    public static List<CropConfig> LoadJsonFile()
+
+    {
+        var file = Directory.GetFiles(BepInEx.Paths.PluginPath, "GrowCropsAnytimeConfig.json",
+            SearchOption.AllDirectories);
+
+        if (file.Length <= 0)
+        {
+            GrowCropsAnytime.Log.LogWarning("No JSON file was found, no crops seasons will be changed.\n Creating example JSON.");
+            CreateSampleJSON();
+            return [];
+        }
+
+        try
+        {
+            var json = File.ReadAllText(file[0]);
+            var cropConfigData = JsonSerializer.Deserialize<List<CropConfig>>(json);
+            return cropConfigData ?? [];
+        }
+        catch (Exception e)
+        {
+            GrowCropsAnytime.Log.LogError($"Error reading/deseralizing JSON. \n{e}");
+            return[];
+        }
+    }
+
+    private static void CreateSampleJSON()
+    {
+        var filePath = Path.Combine(BepInEx.Paths.PluginPath, "GrowCropsAnytimeConfig.json");
+        List<CropConfig> exampleJson =
+        [
+            new CropConfig(105000, true, false, false, false),
+            new CropConfig(105019, false, true, true, false)
+        ];
+        
+        var json = JsonSerializer.Serialize(exampleJson, new JsonSerializerOptions {WriteIndented = true});
+        File.WriteAllText(filePath, json);
+    }
+}
---
+++ GrowCropsAnytime/GrowCropsAnytime.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using BokuMono;
+using HarmonyLib;
+
+namespace GrowCropsAnytime;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class GrowCropsAnytime : BasePlugin
+{
+    internal static new ManualLogSource Log;
+    private static ConfigEntry<bool> customCropSeasons;
+    public static List<CropMasterJSON.CropConfig> _cropConfigs = [];
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        
+        customCropSeasons = Config.Bind("General", "CustomCropConfig", false, "If true, the mod will load a JSON file with custom crop seasons. If false, all crops will grow in all seasons.");
+        if (customCropSeasons.Value)
+        {
+            // Load a JSON for all the custom set values for crop seasons.
+            _cropConfigs = CropMasterJSON.LoadJsonFile();   
+        }
+        Harmony.CreateAndPatchAll(typeof(CropPatch));
+    }
+
+    public class CropPatch
+    {
+        [HarmonyPatch(typeof(UITitleMainPage), "PlayTitleLogoAnimation")]
+        [HarmonyPostfix]
+        public static void PatchCropSeasons()
+        {
+            var cropMaster = MasterDataManager.Instance.CropMaster;
+            var customCrop = customCropSeasons.Value;
+
+            foreach (var cropMasterData in cropMaster.list)
+            {
+                if(cropMasterData == null) continue;
+                if (customCrop)
+                {
+                    // This way is easier since cropMasterData list in il2cpp which doesn't like LINQ
+                    var customCropConfig = _cropConfigs.Find(x => x.CropId == cropMasterData.Id);
+
+                    if (customCropConfig == null) continue;
+                    cropMasterData.GrowInSpring = customCropConfig.GrowInSpring ? 1 : 0;
+                    cropMasterData.GrowInSummer = customCropConfig.GrowInSummer ? 1 : 0;
+                    cropMasterData.GrowInAutumn = customCropConfig.GrowInAutumn ? 1 : 0;
+                    cropMasterData.GrowInWinter = customCropConfig.GrowInWinter ? 1 : 0;
+                    continue;
+                }
+                
+                cropMasterData.GrowInSpring = 1;
+                cropMasterData.GrowInSummer = 1;
+                cropMasterData.GrowInAutumn = 1;
+                cropMasterData.GrowInWinter = 1;
+            }
+        }
+    }
+}
